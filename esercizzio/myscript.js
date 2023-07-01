99e9e2caded4f7fa44fd5c87eac0b5e62afc690a
paoloflore99 <paoloflore937@gmail.com>
2023-07-02 01:35:26 +0200

    mi manca da applicare lo sconto in base all eta

---
+++ esercizzio/myscript.js
@@ -0,0 +1,23 @@
+console.log("bellaaaaa")
+
+alert("domande")
+
+const eta = prompt("inserire la tua eta")
+const chilometri = prompt("inserire il numero di chilometri")
+
+
+document.getElementById("eta").innerHTML = eta
+document.getElementById("chilometri").innerHTML = chilometri
+document.getElementById("prezzoFinale").innerHTML = (chilometri * 0.21)
+
+//document.getElementById("tutto").innerHTML = nome+cognome+preferito+nascita
+
+/*il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.*/
+
+//prezzoFinale
+//prova della formula da fare 
+//console.log((50 * 20) / 100 ) //32.2 e lo sconto applicato
+console.log(chilometri * 0.21) 
+console.log
